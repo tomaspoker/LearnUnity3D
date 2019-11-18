aa42999f55fd5d8c634f45884da9076edd9a8343
tomaspoker <tomaspoker0620@gmail.com>
2019-11-18 15:36:37 +0800

    Timer初版

--- Assets/Scripts/Framework/Timer/Timer.cs
+++ Assets/Scripts/Framework/Timer/Timer.cs
@@ -1,18 +1,166 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
+namespace Boking
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Timer
     {
-        
+        /// <summary>
+        /// 启动时的时间戳
+        /// </summary>
+        private float m_TimestampLaunch;
+
+        /// <summary>
+        /// 总时间
+        /// </summary>
+        private float m_Duration;
+
+        /// <summary>
+        /// 步长-默认1s
+        /// </summary>
+        private float m_Stride;
+
+        /// <summary>
+        /// 当前流失时间
+        /// </summary>
+        private float m_CurrentElapsedTime;
+
+        /// <summary>
+        /// 总流失时间
+        /// </summary>
+        private float m_TotalElapsedTime;
+
+        /// <summary>
+        /// 剩余的时间
+        /// </summary>
+        private float m_RemainingTime;
+
+        /// <summary>
+        /// 是否循环
+        /// </summary>
+        private bool m_IsLoop;
+
+        /// <summary>
+        /// 是否被暂停了
+        /// </summary>
+        private bool m_IsPaused;
+
+        private bool m_IsFirstTikTok;
+
+        /// <summary>
+        /// 倒计时完成的回调
+        /// </summary>
+        private Action m_CallbackCompleted;
+
+        /// <summary>
+        /// 指定时间间隔的回调
+        /// </summary>
+        private Action<float, float> m_CallbackProcess;
+
+        /// <summary>
+        /// 倒计时是否完成
+        /// </summary>
+        public bool IsCompleted { get; set; }
+
+        public bool IsPaused { get; set; }
+
+
+        public Timer(float duration, bool isLoop, Action completed, Action<float, float> process, float stride)
+        {
+            m_Duration = duration;
+            m_IsLoop = isLoop;
+
+            m_CallbackCompleted = completed;
+            m_CallbackProcess = process;
+
+            m_Stride = stride;
+
+            m_IsPaused = false;
+
+            m_IsFirstTikTok = false;
+
+            m_RemainingTime = duration;
+        }
+
+        public static Timer Create(float duration, Action completed)
+        {
+            return new Timer(duration, false, completed, null, 1f);
+        }
+
+        public static Timer Create(float duration, Action completed, Action<float, float> process)
+        {
+            return new Timer(duration, false, completed, process, 1f);
+        }
+
+        public static Timer Create(float duration, bool isLoop, Action<float, float> process)
+        {
+            return new Timer(duration, isLoop, null, process, 1f);
+        }
+
+        public static Timer Create(float duration, bool isLoop, Action completed, Action<float, float> process)
+        {
+            return new Timer(duration, isLoop, completed, process, 1f);
+        }
+
+        public static Timer Create(float duration, bool isLoop, Action<float, float> process, float stride)
+        {
+            return new Timer(duration, isLoop, null, process, stride);
+        }
+
+        public static Timer Create(float duration, bool isLoop, Action completed, Action<float, float> process, float stride)
+        {
+            return new Timer(duration, isLoop, completed, process, stride);
+        }
+
+        public void Update()
+        {
+            if (m_IsFirstTikTok == false)
+            {
+                m_IsFirstTikTok = true;
+                m_CurrentElapsedTime = 0f;
+            }
+            else
+            {
+                m_CurrentElapsedTime += Time.deltaTime;
+
+                if (m_CurrentElapsedTime >= m_Stride)
+                {
+                    m_RemainingTime -= m_Stride;
+
+                    m_CurrentElapsedTime -= m_Stride;
+
+                    m_TotalElapsedTime += m_Stride;
+
+                    m_CallbackProcess?.Invoke(m_RemainingTime, m_TotalElapsedTime);
+                }
+                
+                if (m_RemainingTime <= 0.005f)
+                {
+                    if (m_IsLoop)
+                    {
+                        m_RemainingTime = m_Duration;
+                    }
+                    else
+                    {
+                        IsCompleted = true;
+                    }
+
+                    m_CallbackCompleted?.Invoke();
+                }
+            }
+        }
+
+        public void Pause()
+        {
+            m_IsPaused = true;
+        }
+
+        public void Resume()
+        {
+            m_IsPaused = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Manager/TimerManager.cs
+++ Assets/Scripts/Manager/TimerManager.cs
@@ -1,18 +1,132 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimerManager : MonoBehaviour
+namespace Boking
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TimerManager : BaseSingleton<TimerManager>
     {
+        private static int s_TimerId = 0;
         
+        /// <summary>
+        /// Timer字典
+        /// </summary>
+        private Dictionary<int, Timer> m_TimerDict = new Dictionary<int, Timer>();
+
+        private Dictionary<int, MonoBehaviour> m_TimerBehaviourDict = new Dictionary<int, MonoBehaviour>();
+
+        private int TimerId => s_TimerId++;
+
+        /// <summary>
+        /// 倒计时
+        /// </summary>
+        /// <param name="duration">需要倒计时的时间</param>
+        /// <param name="completed">倒计时完成时的回调</param>
+        /// <returns></returns>
+        public int CountDown(float duration, Action completed)
+        {
+            Timer timer = Timer.Create(duration, completed);
+
+            return Register(timer);
+        }
+
+        /// <summary>
+        /// 倒计时，每次步进均有回调
+        /// </summary>
+        /// <param name="duration">需要倒计时的时间</param>
+        /// <param name="completed">倒计时完成时的回调</param>
+        /// <param name="process">每次步进的回调</param>
+        /// <returns></returns>
+        public int Clock(float duration, Action completed, Action<float, float> process)
+        {
+            Timer timer = Timer.Create(duration, false, completed, process);
+
+            return Register(timer);
+        }
+
+        /// <summary>
+        /// 无限循环的倒计时
+        /// </summary>
+        /// <param name="duration">每隔这么时间步进一次</param>
+        /// <param name="process">每次步进的回调</param>
+        /// <returns></returns>
+        public int Loop(float duration, Action<float, float> process)
+        {
+            Timer timer = Timer.Create(duration, true, process);
+
+            return Register(timer);
+        }
+
+        private int Register(Timer timer)
+        {
+            int id = TimerId;
+
+            m_TimerDict.Add(id, timer);
+
+            return id;
+        }
+
+        public void Update()
+        {
+            foreach(KeyValuePair<int, Timer> vk in m_TimerDict)
+            {
+                if (vk.Value.IsCompleted)
+                {
+                    Remove(vk.Key);
+                }
+                else if (vk.Value.IsPaused)
+                {
+                    // 被暂停了
+                }
+                else
+                {
+                    vk.Value.Update();
+                }
+            }
+        }
+
+        public void Pause(int id)
+        {
+            if (m_TimerDict.TryGetValue(id, out Timer timer))
+            {
+                timer.Pause();
+            }
+        }
+
+        public void Resume(int id)
+        {
+            if (m_TimerDict.TryGetValue(id, out Timer timer))
+            {
+                timer.Resume();
+            }
+        }
+
+        /// <summary>
+        /// 移除一个倒计时
+        /// </summary>
+        /// <param name="id"></param>
+        public void Remove(int id)
+        {
+            m_TimerDict.Remove(id);
+
+            m_TimerBehaviourDict.Remove(id);
+        }
+
+        /// <summary>
+        /// 把定时器与一个脚本绑定在一起
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="behavior"></param>
+        public void Bind(int id, MonoBehaviour behavior)
+        {
+            m_TimerBehaviourDict.Add(id, behavior);
+        }
+
+        public void Adjust()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
