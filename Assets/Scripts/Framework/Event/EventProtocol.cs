0bdaf9073a4648f89a082cdcb7f4efd098bc7db8
tomaspoker <tomaspoker0620@gmail.com>
2019-12-02 18:55:54 +0800

    事件机制

---
+++ Assets/Scripts/Framework/Event/EventProtocol.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boking
+{
+    public delegate void EventListener(params object[] args);
+
+    public interface IEventProtocol
+    {
+        void AddEvent(int key, EventListener listener);
+
+        void RemoveEvent(int key);
+
+        void RemoveEvent(int key, EventListener listener);
+
+        void DispatchEvent(int key, params object[] args);
+    }
+
+    public class EventProtocol : IEventProtocol
+    {
+        private class ListenerWrapper
+        {
+            private List<EventListener> m_ListenerList = new List<EventListener>();
+
+            private int Key { get; }
+
+            public ListenerWrapper(int key)
+            {
+                Key = key;
+            }
+
+            public void Add(EventListener listener)
+            {
+                m_ListenerList.Add(listener);
+            }
+
+            public void Remove(EventListener listener)
+            {
+                m_ListenerList.Remove(listener);
+            }
+
+            public void Dispatch(params object[] args)
+            {
+                foreach(EventListener listener in m_ListenerList)
+                {
+                    listener(args);
+                }
+            }
+        }
+
+        private Dictionary<int, ListenerWrapper> m_EventDict = new Dictionary<int, ListenerWrapper>();
+
+        public void AddEvent(int key, EventListener listener)
+        {
+            if (!m_EventDict.TryGetValue(key, out ListenerWrapper wrapper))
+            {
+                wrapper = new ListenerWrapper(key);
+
+                m_EventDict.Add(key, wrapper);
+            }
+
+            wrapper.Add(listener);
+        }
+
+        public void RemoveEvent(int key)
+        {
+            m_EventDict.Remove(key);
+        }
+
+        public void RemoveEvent(int key, EventListener listener)
+        {
+            if (m_EventDict.TryGetValue(key, out ListenerWrapper wrapper))
+            {
+                wrapper.Remove(listener);
+            }
+        }
+
+        public void DispatchEvent(int key, params object[] args)
+        {
+            if (m_EventDict.TryGetValue(key, out ListenerWrapper wrapper))
+            {
+                wrapper.Dispatch(args);
+            }
+        }
+    }
+}
--- Assets/Scripts/Manager/EventManager.cs
+++ Assets/Scripts/Manager/EventManager.cs
@@ -6,14 +6,31 @@ namespace Boking
 {
     public class EventManager : Singleton<EventManager>
     {
+        private EventProtocol m_Event = new EventProtocol();
+
         private EventManager()
         {
 
         }
 
-        public virtual void Init()
+        public void AddEvent(int key, EventListener listener)
         {
+            m_Event.AddEvent(key, listener);
+        }
 
+        public void RemoveEvent(int key)
+        {
+            m_Event.RemoveEvent(key);
+        }
+
+        public void RemoveEvent(int key, EventListener listener)
+        {
+            m_Event.RemoveEvent(key, listener);
+        }
+
+        public void DispatchEvent(int key, params object[] args)
+        {
+            m_Event.DispatchEvent(key, args);
         }
     }
 
